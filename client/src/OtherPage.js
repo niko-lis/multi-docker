1f02258959a282e49178b86d369a3963c2cd6c55
ΖΕΥΛΗΣ ΝΙΚΟΣ <zevlisn@interamerican.gr>
2025-04-30 20:32:59 +0300

    another minor change

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const OtherPage = () => {
   return (
     <div>
-      Im some other page!
+      Another random page ...
       <Link to="/">Go back home</Link>
     </div>
   );
